ca756aa6c38d7b285a695184860739990f089d8e
Anil kumar mamidala <amami@codeaurora.org>
2014-01-21 12:07:00 +0530

    msm: pm: print active clocks before entering into pc
    print out all active clocks during idle power collapse
    and suspend power collapse using clock debug API
    
    CRs-fixed: 596828
    Change-Id: Ie653c02d490b80de001f61fe8320ee851bb139c7
    Signed-off-by: Anil kumar mamidala <amami@codeaurora.org>

--- arch/arm/mach-msm/msm-pm.c
+++ arch/arm/mach-msm/msm-pm.c
@@ -643,6 +643,14 @@ static enum msm_pm_time_stats_id msm_pm_power_collapse(bool from_idle)
 	if (MSM_PM_DEBUG_POWER_COLLAPSE & msm_pm_debug_mask)
 		pr_info("CPU%u: %s: pre power down\n", cpu, __func__);
 
+	/* This spews a lot of messages when a core is hotplugged. This
+	 * information is most useful from last core going down during
+	 * power collapse
+	 */
+	if ((!from_idle && cpu_online(cpu))
+			|| (MSM_PM_DEBUG_IDLE_CLK & msm_pm_debug_mask))
+		clock_debug_print_enabled();
+
 	avsdscr = avs_get_avsdscr();
 	avscsr = avs_get_avscsr();
 	avs_set_avscsr(0); /* Disable AVS */
