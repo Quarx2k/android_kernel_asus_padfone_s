d3d0e5704a42d70ebc40da291cce18141b89f258
Quarx2k <agent00791@gmail.com>
2014-12-09 23:40:15 +0600

    Disable own freq table

--- arch/arm/mach-msm/clock-krait-8974.c
+++ arch/arm/mach-msm/clock-krait-8974.c
@@ -720,8 +720,8 @@ static int clock_krait_8974_driver_probe(struct platform_device *pdev)
 			"qcom,speed%d-pvs%d-bin-v%d", speed, pvs, pvs_ver);
 
 	// Force own freq table
-	snprintf(table_name, ARRAY_SIZE(table_name), "qcom,speed4-pvs1-bin-v1");
-	printk("table_name %s\n", table_name);
+	//snprintf(table_name, ARRAY_SIZE(table_name), "qcom,speed4-pvs1-bin-v1");
+	//printk("table_name %s\n", table_name);
 
 	rows = parse_tbl(dev, table_name, 3,
 			(u32 **) &freq, (u32 **) &uv, (u32 **) &ua);
