84ba31655a77b3111eab2594079b7b35a7579433
Quarx2k <agent00791@gmail.com>
2015-07-09 16:22:45 +0600

    Remove duplciate smem init

--- arch/arm/mach-msm/asus/board-asus-8974.c
+++ arch/arm/mach-msm/asus/board-asus-8974.c
@@ -212,7 +212,6 @@ static struct platform_device *msm_a91_sensor_devices[] = {
  */
 void __init msm8974_add_drivers(void)
 {
-	msm_smem_init();
 	msm_init_modem_notifier_list();
 	msm_smd_init();
 	msm_rpm_driver_init();
