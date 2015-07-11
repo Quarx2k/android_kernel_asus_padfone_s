7ae3691962616d7bd3d640cef9898fdd4eb3534b
Steve Kondik <steve@cyngn.com>
2015-07-11 05:49:30 -0700

    video: mdss: LiveDisplay driver
     * Provide a simple color temperature adjustment interface via MDSS PCC.
     * Provide hardware integration support for CABC, SRE, and CE.
    
    Change-Id: I4ed1f3876767dd6bedccaf0bbf61af7d36730ca0
    
    Conflicts:
    	drivers/video/msm/mdss/Makefile
    	drivers/video/msm/mdss/mdss_dsi_panel.c

--- drivers/video/msm/mdss/mdss_dsi_panel.c
+++ drivers/video/msm/mdss/mdss_dsi_panel.c
@@ -23,6 +23,7 @@
 #include <linux/err.h>
 
 #include "mdss_dsi.h"
+#include "mdss_livedisplay.h"
 
 #define DT_CMD_HDR 6
 
@@ -199,7 +200,7 @@ u32 mdss_dsi_panel_cmd_read(struct mdss_dsi_ctrl_pdata *ctrl, char cmd0,
 	return 0;
 }
 
-static void mdss_dsi_panel_cmds_send(struct mdss_dsi_ctrl_pdata *ctrl,
+void mdss_dsi_panel_cmds_send(struct mdss_dsi_ctrl_pdata *ctrl,
 			struct dsi_panel_cmds *pcmds)
 {
 	struct dcs_cmd_req cmdreq;
@@ -717,12 +718,12 @@ static int mdss_dsi_panel_on(struct mdss_panel_data *pdata)
 #ifdef ASUS_PF500KL_PROJECT
 #ifdef CONFIG_LEDS_QPNP
 	if (g_ASUS_hwID == A90_EVB || g_ASUS_hwID >= A91_SR1) {
-
 		qpnp_wled_ctrl(1);
 	}
 	himax_ts_resume();
 #endif
 #endif
+	mdss_livedisplay_update(ctrl, MODE_UPDATE_ALL);
 end:
 	pinfo->blank_state = MDSS_PANEL_BLANK_UNBLANK;
 	pr_debug("%s:-\n", __func__);
@@ -845,7 +846,7 @@ static void mdss_dsi_parse_trigger(struct device_node *np, char *trigger,
 }
 
 
-static int mdss_dsi_parse_dcs_cmds(struct device_node *np,
+int mdss_dsi_parse_dcs_cmds(struct device_node *np,
 		struct dsi_panel_cmds *pcmds, char *cmd_key, char *link_key)
 {
 	const char *data;
@@ -1546,6 +1547,8 @@ static int mdss_panel_parse_dt(struct device_node *np,
 	}
 
 	mdss_dsi_parse_panel_horizintal_line_idle(np, ctrl_pdata);
+
+	mdss_livedisplay_parse_dt(np, pinfo);
 
 	return 0;
 
